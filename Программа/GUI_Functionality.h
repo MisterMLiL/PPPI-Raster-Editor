938ebf9b8a4d403c99752d0cbba2e33034fc0078
MisterMLiL <eugenelazurenko@gmail.com>
2024-02-19 11:47:36 +0300

    Добавлен интерфейс модуля функциональности графического интерфейса пользователя

---
+++ Программа/GUI_Functionality.h
@@ -0,0 +1,9 @@
+// Module: Графический интерфейс пользователя
+class GUI_Functionality {
+public:
+    void showMainWindow(); // Метод для отображения главного окна программы
+    void openDocument(); // Метод для открытия документа
+    void saveDocument(); // Метод для сохранения документа
+    void exportDocument(); // Метод для экспорта документа
+    void printDocument(); // Метод для печати документа
+};
