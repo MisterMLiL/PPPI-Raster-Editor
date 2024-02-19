bd86c108e6958a6aad40ce3662ecb9ed8fff6b91
MisterMLiL <eugenelazurenko@gmail.com>
2024-02-19 12:09:39 +0300

    Добавлен интерфейс модуля плагинов и расширений

---
+++ Программа/PluginsAndExtensions.h
@@ -0,0 +1,6 @@
+// Module: Плагины и расширения
+class PluginsAndExtensions {
+public:
+    void addPlugin(); // Метод для добавления плагина
+    void usePlugin(); // Метод для использования плагина
+};
